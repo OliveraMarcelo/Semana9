e65480676c5ebd6c4e4f0ea6d6a6542f29100cb4
Tu Nombre Personal <olivera.m.et13@gmail.com>
2025-05-21 00:14:14 -0300

    FEAT add exercise 3

--- Program.cs
+++ Program.cs
@@ -17,6 +17,63 @@ namespace Semana9
             empleadoTiempoCompleto.calcularSueldo();
             Empleado empleadoMedioTiempo = new EmpleadoMedioTiempo("Maria", 40000, true);
             empleadoMedioTiempo.calcularSueldo();
+            Console.WriteLine("Ejercicio 3 - Vehiculos");
+            Vehiculo[] vehiculos = new Vehiculo[3];
+              vehiculos[0] = new Auto();
+              vehiculos[1] = new Moto();
+              vehiculos[2] = new Camion();
+              foreach (Vehiculo v in vehiculos)
+              {
+                  v.MostrarTipo();
+                  v.Arrancar();
+                  Console.WriteLine();
+              }
+        }
+        public abstract class Vehiculo
+        {
+            public abstract void Arrancar();
+            public abstract void MostrarTipo();
+            public void Acelerar()
+            {
+                Console.WriteLine("Aceleramos o khe!");
+            }
+        }
+        public class Auto : Vehiculo
+        {
+            public override void Arrancar()
+            {
+                Console.WriteLine("Arranca la Auto");
+            }
+
+            public override void MostrarTipo()
+            {
+                Console.WriteLine("Soy un Auto");
+            }
+        }
+        public class Moto : Vehiculo
+        {
+            public override void Arrancar()
+            {
+                Console.WriteLine("Arranca la moto");
+
+
+            }
+
+            public override void MostrarTipo()
+            {
+                Console.WriteLine("Soy una moto");
+            }
+        }
+        public class Camion : Vehiculo
+        {
+            public override void Arrancar()
+            {
+                Console.WriteLine("Arranca Camion");
+            }
+            public override void MostrarTipo()
+            {
+                Console.WriteLine("Soy un camion");
+            }
         }
     }
 }
