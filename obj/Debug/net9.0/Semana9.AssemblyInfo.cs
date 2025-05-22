d02d77ccc1694b6258446f8434a7de6b3e49de5b
Tu Nombre Personal <olivera.m.et13@gmail.com>
2025-05-22 00:53:30 -0300

    FEAT : add exercise 3

--- obj/Debug/net9.0/Semana9.AssemblyInfo.cs
+++ obj/Debug/net9.0/Semana9.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ec7385d586b7d82d20292f12a39c15e0bc439d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcdfc2bc3f72b0e0c242c51d8347e4eb07da0f3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana9")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana9")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
